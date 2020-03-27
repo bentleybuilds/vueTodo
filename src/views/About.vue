2932996ed036a85300bb0b459ec13aec00135625
bentleybuilds <bentleybuilds@gmail.com>
2020-03-27 11:04:09 -0600

    Built out Todo List, calling JSON placeholder API

---
+++ src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>
+      This is the TodoList app created with VUE.JS and a part of the youtube crashcourse
+    </p>
+  </div>
+</template>
